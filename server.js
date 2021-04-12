cb7de4ff5eba4eab2d183048d2e6a5842a6cf117
Matthew Ward <mattrward1030@gmail.com>
2021-04-11 20:03:17 -0400

    added code to syn Sequilize models to the MySQL database on server start

--- server.js
+++ server.js
@@ -14,3 +14,8 @@ app.use(routes);
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
